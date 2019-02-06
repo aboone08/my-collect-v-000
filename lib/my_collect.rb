6afcfe008ff37b60e5c2db3374750291bfd1ce61
Andranique Boone <andranique.boone@gmail.com>
2019-02-06 00:54:13 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   i=0
   modified_collection = []
   while i<collection.length
-    modified_collection<<yield (array[i])
+    modified_collection<<yield (collection[i])
     i=i+1
   end
   modified_collection
