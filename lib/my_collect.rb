7f38017d0b95ebdf1643eda5f7eb4d147dbf6eed
Andranique Boone <andranique.boone@gmail.com>
2019-02-06 00:30:59 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,11 @@
-def my_collect()
+def my_collect(array)
     i=0
   while i<array.length
     yield (array[i])
     i=i+1
   end
-
+    array
+  end
     my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]) do |name|
       return name.split(" ").first
       end
