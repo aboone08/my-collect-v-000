6a301db124ed4938987e400e6da238e15bf30f01
Andranique Boone <andranique.boone@gmail.com>
2019-02-06 00:43:59 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,6 +4,12 @@ def my_collect(array)
   while i<array.length
     yield (array[i])
     i=i+1
+    if my_collect(array) do |name|
+      name.split(" ").first
+      end
+    else my_collect(array) do |lang|
+      lang.upcase
+      end
   end
-  array.upcase
+  array
 end
